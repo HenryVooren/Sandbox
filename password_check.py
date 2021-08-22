97eb771f2b6e10142574dc7eeecd3975398b6991
HenryVooren <henry.vooren@my.jcu.edu.au>
2021-08-22 21:15:47 +1000

    updated with constant variable

--- password_check.py
+++ password_check.py
@@ -3,4 +3,12 @@ CP1404/CP5632 - Practical
 Password checker
 """
 
+MIN_LENGTH = 9
 
+password = input("Enter Password > ")
+while len(password) < MIN_LENGTH:
+    print("Invalid password entered")
+    password = input("Enter Password > ")
+
+for i in range(0, len(password)):
+    print("*", end="")
